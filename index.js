204869af32acd5830c8e85bcdeeae0042630d7fc
aryan <aryanneogi@gmail.com>
2024-05-07 02:04:18 +0530

    Added mongoose

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ import articleRouter from "./routes/blog.js";
 import mongoose from "mongoose";
 import Article from "./models/articles.js";
 import methodOverride from "method-override";
+import { MongoClient,ServerApiVersion } from "mongodb";
 
+const uri = "mongodb+srv://aryanneogi:aryan123@blog-app.z6dxj9x.mongodb.net/?retryWrites=true&w=majority&appName=blog-app";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const app = express();
 const port = 3000;
